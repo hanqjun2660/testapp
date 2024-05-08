6b4226aca5f00def48d9958196d167a24036ef1e
KANG\b2000 <hanqjun2660@gmail.com>
2024-05-08 12:42:44 +0900

    cors 설정

--- src/main/java/com/example/kube_test/config/WebMvcConfig.java
+++ src/main/java/com/example/kube_test/config/WebMvcConfig.java
@@ -14,7 +14,7 @@ public class WebMvcConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET")
                         .allowCredentials(true);
             }
