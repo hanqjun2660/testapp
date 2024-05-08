a9ecb4b0886416d65caa7737fb293fb03e36d675
KANG\b2000 <hanqjun2660@gmail.com>
2024-05-08 15:23:54 +0900

    cors 설정

--- src/main/java/com/example/kube_test/controller/TestController.java
+++ src/main/java/com/example/kube_test/controller/TestController.java
@@ -1,8 +1,10 @@
 package com.example.kube_test.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "https://front.devsj.site", maxAge = 3600)
 @RestController
 public class TestController {
 
