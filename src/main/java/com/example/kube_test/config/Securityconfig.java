4d076ddf91d9a9bf1a22aa2f0fc745c1019248c2
KANG\b2000 <hanqjun2660@gmail.com>
2024-05-08 15:32:21 +0900

    security 설정

---
+++ src/main/java/com/example/kube_test/config/Securityconfig.java
@@ -0,0 +1,38 @@
+package com.example.kube_test.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+public class Securityconfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+                        corsConfiguration.setAllowedOrigins(Collections.singletonList("*"));
+                        corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+                        corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+
+                        corsConfiguration.setExposedHeaders(Collections.singletonList("Access-Control-Allow-Origin"));
+
+                        return corsConfiguration;
+                    }
+                }));
+        
+        return http.build();
+    }
+}
